3405c0987e4c16cfdd9530766569ea00b08325c0
Bryan Nova <bryanherman7@gmail.com>
2018-08-28 17:26:59 -0400

    added MostRecentDateInformationFunctionalTests

--- DataIntegration/Queries/ExtraInformation/DateAttributes/MostRecentDateInformation.swift
+++ DataIntegration/Queries/ExtraInformation/DateAttributes/MostRecentDateInformation.swift
@@ -10,6 +10,9 @@ import Foundation
 
 public class MostRecentDateInformation: AnyInformation {
 
+	fileprivate typealias Me = OldestDateInformation
+	static let noSamplesMessage = "No samples between given start and end dates"
+
 	public override var key: String { get { return "Most Recent" } }
 
 	public required init(_ attribute: Attribute) {
@@ -19,7 +22,7 @@ public class MostRecentDateInformation: AnyInformation {
 	public override func compute(forSamples samples: [Sample]) -> String {
 		let filteredSamples = DependencyInjector.util.sampleUtil.getOnly(samples: samples, from: startDate, to: endDate)
 		if filteredSamples.count == 0 {
-			return "No samples between given start and end dates"
+			return Me.noSamplesMessage
 		}
 
 		var mostRecentDate = try! filteredSamples[0].value(of: attribute) as! Date
