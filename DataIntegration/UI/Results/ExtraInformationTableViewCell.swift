ebd492715609c4eda23928b23f020e28fa7967db
Bryan Nova <bryanherman7@gmail.com>
2018-09-21 09:58:23 -0400

    made display of information on results screen more consistent

--- DataIntegration/UI/Results/ExtraInformationTableViewCell.swift
+++ DataIntegration/UI/Results/ExtraInformationTableViewCell.swift
@@ -10,27 +10,14 @@ import UIKit
 
 final class ExtraInformationTableViewCell: UITableViewCell {
 
-	public final var extraInformation: ExtraInformation! {
-		didSet {
-			if extraInformation == nil { return }
-			var text = ""
-			if value != nil {
-				text = value
-			}
-			keyValueLabel.text = extraInformation.description + ": " + text
-		}
-	}
+	public final var extraInformation: ExtraInformation! { didSet { updateText() } }
 
-	public final var value: String! {
-		didSet {
-			if value == nil { return }
-			var text = ""
-			if extraInformation != nil {
-				text = extraInformation.description + ": "
-			}
-			keyValueLabel.text = text + value
-		}
-	}
+	public final var value: String! { didSet { updateText() } }
 
 	@IBOutlet weak final var keyValueLabel: UILabel!
+
+	private final func updateText() {
+		if value == nil || extraInformation == nil { return }
+		keyValueLabel.text = extraInformation.description.localizedCapitalized + ": " + value
+	}
 }
