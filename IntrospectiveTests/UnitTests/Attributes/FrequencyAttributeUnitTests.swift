f45c28606c06dbd6efcdc2fb0691f6621025eab5
Bryan Nova <bryanherman7@gmail.com>
2019-03-25 11:12:03 -0400

    277 - add FrequencyAttributeUnitTests

---
+++ IntrospectiveTests/UnitTests/Attributes/FrequencyAttributeUnitTests.swift
@@ -0,0 +1,192 @@
+//
+//  FrequencyAttributeUnitTests.swift
+//  IntrospectiveTests
+//
+//  Created by Bryan Nova on 3/25/19.
+//  Copyright © 2019 Bryan Nova. All rights reserved.
+//
+
+import XCTest
+import Hamcrest
+@testable import Introspective
+
+final class FrequencyAttributeUnitTests: UnitTest {
+
+	private final var attribute: FrequencyAttribute!
+
+	// MARK: - convertToDisplayableString()
+
+	func testGivenNonFrequencyValue_convertToDisplayableString_throwsTypeMismatchError() {
+		// given
+		let value = "abc"
+		useOptionalAttribute()
+
+		// when
+		XCTAssertThrowsError(try attribute.convertToDisplayableString(from: value)) { error in
+			// then
+			assertThat(error, instanceOf(TypeMismatchError.self))
+		}
+	}
+
+	func testGivenNilValueAndAttributeIsRequired_convertToDisplayableString_throwsTypeMismatchError() {
+		// given
+		useRequiredAttribute()
+
+		// when
+		XCTAssertThrowsError(try attribute.convertToDisplayableString(from: nil)) { error in
+			// then
+			assertThat(error, instanceOf(UnsupportedValueError.self))
+		}
+	}
+
+	func testGivenNilValueAndAttributeIsOptional_convertToDisplayableString_returnsEmptyString() throws {
+		// given
+		useOptionalAttribute()
+
+		// when
+		let displayableString = try attribute.convertToDisplayableString(from: nil)
+
+		// then
+		XCTAssertEqual(displayableString, "")
+	}
+
+	func testGivenFrequency_convertToDisplayableString_returnsCorrectValue() throws {
+		// given
+		useRequiredAttribute()
+		let value = Frequency(2, .hour)
+
+		// when
+		let displayableString = try attribute.convertToDisplayableString(from: value)
+
+		// then
+		XCTAssertEqual(displayableString.localizedLowercase, "2 times per hour")
+	}
+
+	// MARK: - equalTo()
+
+	func testGivenWrongClass_equalTo_returnsFalse() {
+		// given
+		useRequiredAttribute()
+		let other = DoubleAttribute(name: "name")
+
+		// when
+		let areEqual = attribute.equalTo(other)
+
+		// then
+		XCTAssertFalse(areEqual)
+	}
+
+	func testGivenSameObjectTwice_equalTo_returnsTrue() {
+		// given
+		useRequiredAttribute()
+
+		// when
+		let areEqual = attribute.equalTo(attribute)
+
+		// then
+		XCTAssert(areEqual)
+	}
+
+	func testGivenNameMismatch_equalTo_returnsFalse() {
+		// given
+		useRequiredAttribute()
+		let other = FrequencyAttribute(
+			name: attribute.name + "other stuff",
+			pluralName: attribute.pluralName,
+			description: attribute.extendedDescription,
+			variableName: attribute.variableName,
+			optional: attribute.optional)
+
+		// when
+		let areEqual = attribute.equalTo(other)
+
+		// then
+		XCTAssertFalse(areEqual)
+	}
+
+	func testGivenDescriptionMismatch_equalTo_returnsFalse() {
+		// given
+		useRequiredAttribute()
+		let other = FrequencyAttribute(
+			name: attribute.name,
+			pluralName: attribute.pluralName,
+			description: (attribute.extendedDescription ?? "") + "other stuff",
+			variableName: attribute.variableName,
+			optional: attribute.optional)
+
+		// when
+		let areEqual = attribute.equalTo(other)
+
+		// then
+		XCTAssertFalse(areEqual)
+	}
+
+	func testGivenVariableNameMismatch_equalTo_returnsFalse() {
+		// given
+		useRequiredAttribute()
+		let other = FrequencyAttribute(
+			name: attribute.name,
+			pluralName: attribute.pluralName,
+			description: attribute.extendedDescription,
+			variableName: (attribute.variableName ?? "") + "other stuff",
+			optional: attribute.optional)
+
+		// when
+		let areEqual = attribute.equalTo(other)
+
+		// then
+		XCTAssertFalse(areEqual)
+	}
+
+	func testGivenOptionalMismatch_equalTo_returnsFalse() {
+		// given
+		useRequiredAttribute()
+		let other = FrequencyAttribute(
+			name: attribute.name,
+			pluralName: attribute.pluralName,
+			description: attribute.extendedDescription,
+			variableName: attribute.variableName,
+			optional: !attribute.optional)
+
+		// when
+		let areEqual = attribute.equalTo(other)
+
+		// then
+		XCTAssertFalse(areEqual)
+	}
+
+	func testGivenAttributeWithSameProperties_equalTo_returnsTrue() {
+		// given
+		useRequiredAttribute()
+		let other = FrequencyAttribute(
+			name: attribute.name,
+			pluralName: attribute.pluralName,
+			description: attribute.extendedDescription,
+			variableName: attribute.variableName,
+			optional: attribute.optional)
+
+		// when
+		let areEqual = attribute.equalTo(other)
+
+		// then
+		XCTAssert(areEqual)
+	}
+
+	// MARK: - Helper Functions
+
+	private final func useOptionalAttribute() {
+		attribute = FrequencyAttribute(
+			name: "name",
+			description: "description",
+			variableName: "variableName",
+			optional: true)
+	}
+
+	private final func useRequiredAttribute() {
+		attribute = FrequencyAttribute(
+			name: "name",
+			description: "description",
+			variableName: "variableName",
+			optional: false)
+	}
+}
