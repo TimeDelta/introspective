5be98ed5a7ae31478263f1540b6c5f489f312317
Bryan Nova <bryanherman7@gmail.com>
2019-03-26 10:33:28 -0400

    300 - add more tests for MostRecentDateInformation

--- IntrospectiveTests/FunctionalTests/ExtraInformation/DateAttributes/MostRecentDateInformationFunctionalTests.swift
+++ IntrospectiveTests/FunctionalTests/ExtraInformation/DateAttributes/MostRecentDateInformationFunctionalTests.swift
@@ -7,6 +7,7 @@
 //
 
 import XCTest
+import Hamcrest
 import SwiftDate
 @testable import Introspective
 
@@ -20,6 +21,17 @@ final class MostRecentDateInformationFunctionalTests: FunctionalTest {
 	final override func setUp() {
 		super.setUp()
 		information = MostRecentDateInformation(Me.attribute)
+	}
+
+	// MARK: - description
+
+	func test_description_containsRestrictedAttributeName() {
+		// when
+		let description = information.description
+
+		// then
+		let attributeName = Me.attribute.name.localizedLowercase
+		assertThat(description.localizedLowercase, containsString(attributeName))
 	}
 
 	// MARK: - compute()
@@ -404,6 +416,49 @@ final class MostRecentDateInformationFunctionalTests: FunctionalTest {
 			// then
 			XCTAssert(error is DisplayableError)
 		}
+	}
+
+	// MARK: - equalTo()
+
+	func testGivenSameObject_equalTo_returnsTrue() {
+		// when
+		let areEqual = information.equalTo(information)
+
+		// then
+		XCTAssert(areEqual)
+	}
+
+	func testGivenWrongClass_equalTo_returnsFalse() {
+		// given
+		let other = MinimumInformation<Int>(Me.attribute)
+
+		// when
+		let areEqual = information.equalTo(other)
+
+		// then
+		XCTAssertFalse(areEqual)
+	}
+
+	func testGivenDifferentRestrictedAttribute_equalTo_returnsFalse() {
+		// given
+		let other = MostRecentDateInformation(TextAttribute(name: "different attribute"))
+
+		// when
+		let areEqual = information.equalTo(other)
+
+		// then
+		XCTAssertFalse(areEqual)
+	}
+
+	func testGivenSameRestrictedAttribute_equalTo_returnsTrue() {
+		// given
+		let other = MostRecentDateInformation(Me.attribute)
+
+		// when
+		let areEqual = information.equalTo(other)
+
+		// then
+		XCTAssert(areEqual)
 	}
 
 	// MARK: - Helper Functions
