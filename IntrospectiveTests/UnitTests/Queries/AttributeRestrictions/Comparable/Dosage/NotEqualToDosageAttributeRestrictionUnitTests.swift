114140628f45dcc510e810eac5ea76743bee4d03
Bryan Nova <bryanherman7@gmail.com>
2019-03-27 13:05:09 -0400

    add more tests for NotEqualToDosageAttributeRestriction

--- IntrospectiveTests/UnitTests/Queries/AttributeRestrictions/Comparable/Dosage/NotEqualToDosageAttributeRestrictionUnitTests.swift
+++ IntrospectiveTests/UnitTests/Queries/AttributeRestrictions/Comparable/Dosage/NotEqualToDosageAttributeRestrictionUnitTests.swift
@@ -7,10 +7,11 @@
 //
 
 import XCTest
+import Hamcrest
 import SwiftyMocky
 @testable import Introspective
 
-class NotEqualToDosageAttributeRestrictionUnitTests: UnitTest {
+final class NotEqualToDosageAttributeRestrictionUnitTests: UnitTest {
 
 	private typealias Me = NotEqualToDosageAttributeRestrictionUnitTests
 	private static let valueAttribute = NotEqualToDosageAttributeRestriction.valueAttribute
@@ -21,6 +22,28 @@ class NotEqualToDosageAttributeRestrictionUnitTests: UnitTest {
 	final override func setUp() {
 		super.setUp()
 		restriction = NotEqualToDosageAttributeRestriction(restrictedAttribute: Me.restrictedAttribute)
+	}
+
+	// MARK: - description
+
+	func test_description_containsValue() {
+		// given
+		let value = Dosage(2, "mg")
+		restriction.value = value
+
+		// when
+		let description = restriction.description
+
+		// then
+		assertThat(description, containsString(value.description))
+	}
+
+	func test_description_containsNotEqualTo() {
+		// when
+		let description = restriction.description
+
+		// then
+		assertThat(description, containsString("≠"))
 	}
 
 	// MARK: - value(of:)
@@ -130,6 +153,80 @@ class NotEqualToDosageAttributeRestrictionUnitTests: UnitTest {
 		XCTAssert(samplePasses)
 	}
 
+	func testGivenNilSampleValueAndValueIsNotNil_samplePasses_returnsTrue() throws {
+		// given
+		restriction.value = Dosage(2, "mg") as Any
+		let sample = SampleCreatorTestUtil.createSample(withValue: nil as Any?, for: Me.restrictedAttribute)
+
+		// when
+		let samplePasses = try restriction.samplePasses(sample)
+
+		// then
+		XCTAssert(samplePasses)
+	}
+
+	func testGivenNilSampleValueAndValueIsNil_samplePasses_returnsFalse() throws {
+		// given
+		restriction.value = nil as Any?
+		let sample = SampleCreatorTestUtil.createSample(withValue: nil as Any?, for: Me.restrictedAttribute)
+
+		// when
+		let samplePasses = try restriction.samplePasses(sample)
+
+		// then
+		XCTAssertFalse(samplePasses)
+	}
+
+	// MARK: - ==
+
+	func testGivenSameObjectTwice_equalToOperator_returnsTrue() {
+		// when
+		let equal = restriction == restriction
+
+		// then
+		XCTAssert(equal)
+	}
+
+	func testGivenSameClassWithDifferentAttributes_equalToOperator_returnsFalse() {
+		// given
+		let other = NotEqualToDosageAttributeRestriction(
+			restrictedAttribute: DosageAttribute(name: "not the same attribute"))
+
+		// when
+		let equal = restriction == other
+
+		// then
+		XCTAssertFalse(equal)
+	}
+
+	func testGivenSameClassWithSameRestrictedAttributeButDifferentValues_equalToOperator_returnsFalse() {
+		// given
+		let restrictionValue = restriction.value as! Dosage
+		let other = NotEqualToDosageAttributeRestriction(
+			restrictedAttribute: restriction.restrictedAttribute,
+			value: Dosage(restrictionValue.amount + 1, restrictionValue.unit))
+
+		// when
+		let equal = restriction == other
+
+		// then
+		XCTAssertFalse(equal)
+	}
+
+	func testGivenSameTypeWithAllSameAttributes_equalToOperator_returnsTrue() {
+		// given
+		let restrictionValue = restriction.value as! Dosage
+		let other = NotEqualToDosageAttributeRestriction(
+			restrictedAttribute: restriction.restrictedAttribute,
+			value: restrictionValue)
+
+		// when
+		let equal = restriction == other
+
+		// then
+		XCTAssert(equal)
+	}
+
 	// MARK: - equalTo(attributed:)
 
 	func testGivenOtherOfDifferentType_equalToAttributed_returnsFalse() {
@@ -153,7 +250,8 @@ class NotEqualToDosageAttributeRestrictionUnitTests: UnitTest {
 
 	func testGivenSameClassWithDifferentRestrictedAttribute_equalToAttributed_returnsFalse() {
 		// given
-		let otherAttributed: Attributed = NotEqualToDosageAttributeRestriction(restrictedAttribute: DosageAttribute(name: "not the same attribute"))
+		let otherAttributed: Attributed = NotEqualToDosageAttributeRestriction(
+			restrictedAttribute: DosageAttribute(name: "not the same attribute"))
 
 		// when
 		let equal = restriction.equalTo(otherAttributed)
@@ -165,7 +263,9 @@ class NotEqualToDosageAttributeRestrictionUnitTests: UnitTest {
 	func testGivenSameClassWithSameRestrictedAttributeButDifferentValue_equalToAttributed_returnsFalse() {
 		// given
 		let restrictionValue = restriction.value as! Dosage
-		let otherAttributed: Attributed = NotEqualToDosageAttributeRestriction(restrictedAttribute: restriction.restrictedAttribute, value: Dosage(restrictionValue.amount + 1, restrictionValue.unit))
+		let otherAttributed: Attributed = NotEqualToDosageAttributeRestriction(
+			restrictedAttribute: restriction.restrictedAttribute,
+			value: Dosage(restrictionValue.amount + 1, restrictionValue.unit))
 
 		// when
 		let equal = restriction.equalTo(otherAttributed)
@@ -177,7 +277,9 @@ class NotEqualToDosageAttributeRestrictionUnitTests: UnitTest {
 	func testGivenSameTypeWithAllSameAttributes_equalToAttributed_returnsTrue() {
 		// given
 		let restrictionValue = restriction.value as! Dosage
-		let otherAttributed: Attributed = NotEqualToDosageAttributeRestriction(restrictedAttribute: restriction.restrictedAttribute, value: restrictionValue)
+		let otherAttributed: Attributed = NotEqualToDosageAttributeRestriction(
+			restrictedAttribute: restriction.restrictedAttribute,
+			value: restrictionValue)
 
 		// when
 		let equal = restriction.equalTo(otherAttributed)
@@ -190,10 +292,11 @@ class NotEqualToDosageAttributeRestrictionUnitTests: UnitTest {
 
 	func testGivenOtherOfDifferentType_equalToRestriction_returnsFalse() {
 		// given
-		let otherAttributed: AttributeRestriction = ContainsStringAttributeRestriction(restrictedAttribute: restriction.restrictedAttribute)
+		let otherRestriction: AttributeRestriction = ContainsStringAttributeRestriction(
+			restrictedAttribute: restriction.restrictedAttribute)
 
 		// when
-		let equal = restriction.equalTo(otherAttributed)
+		let equal = restriction.equalTo(otherRestriction)
 
 		// then
 		XCTAssertFalse(equal)
@@ -209,10 +312,11 @@ class NotEqualToDosageAttributeRestrictionUnitTests: UnitTest {
 
 	func testGivenSameClassWithDifferentAttributes_equalToRestriction_returnsFalse() {
 		// given
-		let otherAttributed: AttributeRestriction = NotEqualToDosageAttributeRestriction(restrictedAttribute: DosageAttribute(name: "not the same attribute"))
+		let otherRestriction: AttributeRestriction = NotEqualToDosageAttributeRestriction(
+			restrictedAttribute: DosageAttribute(name: "not the same attribute"))
 
 		// when
-		let equal = restriction.equalTo(otherAttributed)
+		let equal = restriction.equalTo(otherRestriction)
 
 		// then
 		XCTAssertFalse(equal)
@@ -221,10 +325,12 @@ class NotEqualToDosageAttributeRestrictionUnitTests: UnitTest {
 	func testGivenSameClassWithSameRestrictedAttributeButDifferentValues_equalToRestriction_returnsFalse() {
 		// given
 		let restrictionValue = restriction.value as! Dosage
-		let otherAttributed: AttributeRestriction = NotEqualToDosageAttributeRestriction(restrictedAttribute: restriction.restrictedAttribute, value: Dosage(restrictionValue.amount + 1, restrictionValue.unit))
+		let otherRestriction: AttributeRestriction = NotEqualToDosageAttributeRestriction(
+			restrictedAttribute: restriction.restrictedAttribute,
+			value: Dosage(restrictionValue.amount + 1, restrictionValue.unit))
 
 		// when
-		let equal = restriction.equalTo(otherAttributed)
+		let equal = restriction.equalTo(otherRestriction)
 
 		// then
 		XCTAssertFalse(equal)
@@ -233,10 +339,12 @@ class NotEqualToDosageAttributeRestrictionUnitTests: UnitTest {
 	func testGivenSameTypeWithAllSameAttributes_equalToRestriction_returnsTrue() {
 		// given
 		let restrictionValue = restriction.value as! Dosage
-		let otherAttributed: AttributeRestriction = NotEqualToDosageAttributeRestriction(restrictedAttribute: restriction.restrictedAttribute, value: restrictionValue)
+		let otherRestriction: AttributeRestriction = NotEqualToDosageAttributeRestriction(
+			restrictedAttribute: restriction.restrictedAttribute,
+			value: restrictionValue)
 
 		// when
-		let equal = restriction.equalTo(otherAttributed)
+		let equal = restriction.equalTo(otherRestriction)
 
 		// then
 		XCTAssert(equal)
@@ -254,10 +362,11 @@ class NotEqualToDosageAttributeRestrictionUnitTests: UnitTest {
 
 	func testGivenSameClassWithDifferentAttributes_equalTo_returnsFalse() {
 		// given
-		let otherAttributed = NotEqualToDosageAttributeRestriction(restrictedAttribute: DosageAttribute(name: "not the same attribute"))
+		let other = NotEqualToDosageAttributeRestriction(
+			restrictedAttribute: DosageAttribute(name: "not the same attribute"))
 
 		// when
-		let equal = restriction.equalTo(otherAttributed)
+		let equal = restriction.equalTo(other)
 
 		// then
 		XCTAssertFalse(equal)
@@ -266,10 +375,12 @@ class NotEqualToDosageAttributeRestrictionUnitTests: UnitTest {
 	func testGivenSameClassWithSameRestrictedAttributeButDifferentValues_equalTo_returnsFalse() {
 		// given
 		let restrictionValue = restriction.value as! Dosage
-		let otherAttributed = NotEqualToDosageAttributeRestriction(restrictedAttribute: restriction.restrictedAttribute, value: Dosage(restrictionValue.amount + 1, restrictionValue.unit))
+		let other = NotEqualToDosageAttributeRestriction(
+			restrictedAttribute: restriction.restrictedAttribute,
+			value: Dosage(restrictionValue.amount + 1, restrictionValue.unit))
 
 		// when
-		let equal = restriction.equalTo(otherAttributed)
+		let equal = restriction.equalTo(other)
 
 		// then
 		XCTAssertFalse(equal)
@@ -278,10 +389,12 @@ class NotEqualToDosageAttributeRestrictionUnitTests: UnitTest {
 	func testGivenSameTypeWithAllSameAttributes_equalTo_returnsTrue() {
 		// given
 		let restrictionValue = restriction.value as! Dosage
-		let otherAttributed = NotEqualToDosageAttributeRestriction(restrictedAttribute: restriction.restrictedAttribute, value: restrictionValue)
+		let other = NotEqualToDosageAttributeRestriction(
+			restrictedAttribute: restriction.restrictedAttribute,
+			value: restrictionValue)
 
 		// when
-		let equal = restriction.equalTo(otherAttributed)
+		let equal = restriction.equalTo(other)
 
 		// then
 		XCTAssert(equal)
