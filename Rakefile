9e7bde4d7616fb6b87bd7aab63de194be9b5b3d7
Bryan Nova <bryanherman7@gmail.com>
2020-08-04 17:22:02 -0400

    update mock generation to latest version of SwiftyMocky

--- Rakefile
+++ Rakefile
@@ -1,10 +1,10 @@
 # Rakefile
 task :mock do
   sh "./gen-swifty-mocky-config-files.sh"
-  sh "Pods/Sourcery/bin/Sourcery.app/Contents/MacOS/Sourcery --config mocky.yml"
+  sh "swiftymocky generate"
 end
 
 task :mockwatch do
   sh "./gen-swifty-mocky-config-files.sh"
-  sh "Pods/Sourcery/bin/Sourcery.app/Contents/MacOS/Sourcery --config mocky.yml --watch"
+  sh "swiftymocky generate --watch"
 end
--- gen-swifty-mocky-config-files.sh
+++ gen-swifty-mocky-config-files.sh
@@ -6,8 +6,7 @@ OUTPUT_DIR="${PROJECT_DIR}/${PROJECT_NAME}Tests"
 NORMAL_MOCKS_FILE="${OUTPUT_DIR}/Mocks.swift"
 echo "Mocks Input Directory = ${INPUT_DIR}"
 
-addArgs() {
-    echo "args:" >> "$1"
+addImports() {
     echo "  testable:" >> "$1"
     echo "    - Introspective" >> "$1"
     echo "    - AttributeRestrictions" >> "$1"
@@ -33,11 +32,6 @@ addArgs() {
     echo "    - SwiftDate" >> "$1"
     echo "    - UserNotifications" >> "$1"
     echo "    - Instructions" >> "$1"
-    echo "  excludedSwiftLintRules:" >> "$1"
-    echo "    - force_cast" >> "$1"
-    echo "    - function_body_length" >> "$1"
-    echo "    - line_length" >> "$1"
-    echo "    - vertical_whitespace" >> "$1"
 }
 
 EXCLUSION_PATTERN="Delegate|\
@@ -47,20 +41,23 @@ DateExtension"
 OLD_IFS="$IFS"
 export IFS=$'\n'
 
-CONFIG_FILE="${PROJECT_DIR}/mocky.yml"
+CONFIG_FILE="${PROJECT_DIR}/Mockfile"
 
-echo "sources:" > "$CONFIG_FILE"
-echo "  - ${OUTPUT_DIR}/CustomMocks" >> "$CONFIG_FILE"
+echo "sourceryCommand: null" > "$CONFIG_FILE"
+echo "sourceryTemplate: null" >> "$CONFIG_FILE"
+echo "IntrospectiveTests:" >> "$CONFIG_FILE"
+echo "  sources:" >> "$CONFIG_FILE"
+echo "    - ${OUTPUT_DIR}/CustomMocks" >> "$CONFIG_FILE"
 for file in `find "${INPUT_DIR}" -type f -name \*.swift | grep -v "/Pods/" | grep -v Carthage | egrep -v "${EXCLUSION_PATTERN}"`; do
-    echo "  - $file" >> "$CONFIG_FILE"
+    echo "    - $file" >> "$CONFIG_FILE"
 done
 
-echo "templates:" >> "$CONFIG_FILE"
-echo "  - $PROJECT_DIR/Pods/SwiftyMocky/Templates" >> "$CONFIG_FILE"
-echo "output:" >> "$CONFIG_FILE"
-echo "  $NORMAL_MOCKS_FILE" >> "$CONFIG_FILE"
+echo "  output: $NORMAL_MOCKS_FILE" >> "$CONFIG_FILE"
 
-addArgs "$CONFIG_FILE"
+echo "  targets:" >> "$CONFIG_FILE"
+echo "    - IntrospectiveTests" >> "$CONFIG_FILE"
+
+addImports "$CONFIG_FILE"
 
 echo "Done creating config files"
 
