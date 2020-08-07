16a8b2ca7e26b11e902493086129a01ad66a577e
Bryan Nova <bryanherman7@gmail.com>
2020-08-07 18:01:41 -0400

    fix mock generation

--- scripts/gen-swifty-mocky-config-files.sh
+++ scripts/gen-swifty-mocky-config-files.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
+set -e
+
 PROJECT_DIR="`git rev-parse --show-toplevel`"
-PROJECT_NAME="`basename "${PROJECT_DIR}"`"
-INPUT_DIR="${PROJECT_DIR}"
+PROJECT_NAME="Introspective"
 OUTPUT_DIR="${PROJECT_DIR}/${PROJECT_NAME}Tests"
 NORMAL_MOCKS_FILE="${OUTPUT_DIR}/Mocks.swift"
-echo "Mocks Input Directory = ${INPUT_DIR}"
 
 addImports() {
     echo "  testable:" >> "$1"
@@ -36,7 +36,8 @@ addImports() {
 
 EXCLUSION_PATTERN="Delegate|\
 Mock|\
-DateExtension"
+DateExtension|\
+IntrospectiveTests"
 
 OLD_IFS="$IFS"
 export IFS=$'\n'
@@ -45,20 +46,22 @@ CONFIG_FILE="${PROJECT_DIR}/Mockfile"
 
 echo "sourceryCommand: null" > "$CONFIG_FILE"
 echo "sourceryTemplate: null" >> "$CONFIG_FILE"
+echo >> "$CONFIG_FILE"
 echo "IntrospectiveTests:" >> "$CONFIG_FILE"
 echo "  sources:" >> "$CONFIG_FILE"
-echo "    - ${OUTPUT_DIR}/CustomMocks" >> "$CONFIG_FILE"
-for file in `find "${INPUT_DIR}" -type f -name \*.swift | grep -v "/Pods/" | grep -v Carthage | egrep -v "${EXCLUSION_PATTERN}"`; do
-    echo "    - $file" >> "$CONFIG_FILE"
+echo "    include:" >> "$CONFIG_FILE"
+echo "      - ./${OUTPUT_DIR#${PROJECT_DIR}/}/CustomMocks" >> "$CONFIG_FILE"
+for file in `find "${PROJECT_DIR}" -type f -name \*.swift | grep -v "/Pods/" | grep -v Carthage | egrep -v "${EXCLUSION_PATTERN}"`; do
+    echo "      - ./${file#${PROJECT_DIR}/}" >> "$CONFIG_FILE"
 done
 
-echo "  output: $NORMAL_MOCKS_FILE" >> "$CONFIG_FILE"
+echo "  output: ./${NORMAL_MOCKS_FILE#${PROJECT_DIR}/}" >> "$CONFIG_FILE"
 
 echo "  targets:" >> "$CONFIG_FILE"
 echo "    - IntrospectiveTests" >> "$CONFIG_FILE"
 
 addImports "$CONFIG_FILE"
 
-echo "Done creating config files"
+echo "Done creating $CONFIG_FILE"
 
 export IFS="$OLD_IFS"
