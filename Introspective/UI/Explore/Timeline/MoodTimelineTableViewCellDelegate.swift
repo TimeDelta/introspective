b9d8a0bf413ce779afac84cd16c64e020a9632b5
Bryan Nova <bryanherman7@gmail.com>
2021-03-19 13:37:36 -0400

    add ability to edit moods from timeline

--- Introspective/UI/Explore/Timeline/MoodTimelineTableViewCellDelegate.swift
+++ Introspective/UI/Explore/Timeline/MoodTimelineTableViewCellDelegate.swift
@@ -8,10 +8,29 @@
 
 import UIKit
 
+import Common
+import DependencyInjection
 import Samples
 
 class MoodTimelineTableViewCellDelegate: TimelineTableViewCellDelegate {
+	private typealias Me = MoodTimelineTableViewCellDelegate
+
+	private static let log = Log()
+
 	func editController(for sample: Sample) -> UIViewController? {
-		nil
+		guard let mood = sample as? Mood else {
+			Me.log.error(
+				"Wrong type of sample passed to retrieve edit controller for mood: %@",
+				sample.attributedName
+			)
+			return nil
+		}
+		let controller: EditMoodTableViewController = injected(UiUtil.self).controller(
+			named: "editMood",
+			from: "Util",
+			as: EditMoodTableViewControllerImpl.self
+		)
+		controller.mood = mood
+		return controller
 	}
 }
